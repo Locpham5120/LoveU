8b890ceb2b2e042d874b7446f72e8c99806b2772
locpx20 <locpxhe140601@fpt.edu.vn>
2023-03-27 21:44:11 +0700

    LoveU
    Datting demo

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Anh xin làm sóng biếc
+                Hôn mãi cát vàng em
+                Hôn thật khẽ, thật êm
+                Hôn êm đềm mãi mãi`,
+    btnIntro: 'Love U',
+    title: 'HH ngồi nhớ VB 😙',
+    desc: 'Tương tư là bệnh của tôi yêu nàng <3',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Em ơi, em à, em có nhớ anh không? <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Em nhớ anh như đông về nhớ rét  <33333333',
+    mess: 'Trời còn có bữa sao quên mọc. Anh chẳng đêm nào chẳng nhớ em😘😘',
+    messDesc: 'Đợi dịp nào anh về qua, cùng em đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.youtube.com/watch?v=UKUzB6LhoYU' 
+}
