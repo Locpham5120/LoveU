ef78d26b173ccc55ce60ebb28f708e7c09f8ff1c
Phạm Lộc <locpxhe140601@fpt.edu.vn>
2023-09-30 23:24:50 +0700

    update code

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
                 Hôn thật khẽ, thật êm
                 Hôn êm đềm mãi mãi`,
     btnIntro: 'Love U',
-    title: 'HH ngồi nhớ VB 😙',
+    title: 'Thôn Đoài ngồi nhớ thôn Đông 😙',
     desc: 'Tương tư là bệnh của tôi yêu nàng <3',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Thích anh chứ <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Em ơi, em à, em có nhớ anh không? <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Em nhớ anh như đông về nhớ rét  <33333333',
     mess: 'Trời còn có bữa sao quên mọc. Anh chẳng đêm nào chẳng nhớ em😘😘',
-    messDesc: 'Đợi dịp nào anh về qua, cùng em đi chơi nha.',
+    messDesc: 'Hẹn em tuần sau mình găp nhau nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.youtube.com/watch?v=UKUzB6LhoYU' 
 }
